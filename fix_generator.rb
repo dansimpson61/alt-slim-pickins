0eb3cb6b56f43f0208ffd49debb8c0195e6826b4
dan simpson <dansimpson61@gmail.com>
2026-09-05 11:20:50 -0400

    chore: remove temporary scripts

--- fix_generator.rb
+++
@@ -1,8 +0,0 @@
-code = File.read("lib/slim_pickins/generator.rb")
-
-code.gsub!(/@out << '<thead>'/, "open_tag('thead')")
-code.gsub!(/@out << '<tbody>'/, "open_tag('tbody')")
-code.gsub!(/@out << '<tfoot>'/, "open_tag('tfoot')")
-code.gsub!(/@out << '<tr>'/, "open_tag('tr')")
-
-File.write("lib/slim_pickins/generator.rb", code)
